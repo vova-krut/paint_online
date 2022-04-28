50c12aa1e786d71059573c9ad7b6a5e81a359339
Volodymyr <vthehang@gmail.com>
2022-04-28 20:45:39 +0300

    added other btns and color picker

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -5,6 +5,14 @@ const ToolBar = () => {
     return (
         <div className="toolbar">
             <button className="toolbar__btn brush" />
+            <button className="toolbar__btn rect" />
+            <button className="toolbar__btn circle" />
+            <button className="toolbar__btn eraser" />
+            <button className="toolbar__btn line" />
+            <input style={{ marginLeft: 10 }} type="color" />
+            <button className="toolbar__btn undo" />
+            <button className="toolbar__btn redo" />
+            <button className="toolbar__btn save" />
         </div>
     );
 };
