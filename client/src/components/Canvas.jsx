93d15661ae9dea46e0a703d4cc8af27b01c317f4
Volodymyr <vthehang@gmail.com>
2022-04-28 20:57:06 +0300

    added state tracking for canvas

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "../style/canvas.scss";
+import { observer } from "mobx-react-lite";
+import canvasState from "../store/canvasState";
 
-const Canvas = () => {
+const Canvas = observer(() => {
+    const canvasRef = useRef();
+
+    useEffect(() => {
+        canvasState.setCanvas(canvasRef.current);
+    }, []);
+
     return (
         <div className="canvas">
-            <canvas width={600} height={400}></canvas>
+            <canvas ref={canvasRef} width={600} height={400}></canvas>
         </div>
     );
-};
+});
 
 export default Canvas;
