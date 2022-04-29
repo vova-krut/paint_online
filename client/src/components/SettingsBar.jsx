54889e97ec855a2f8c550487157725ba8ed607cb
Volodymyr <vthehang@gmail.com>
2022-04-29 21:07:40 +0300

    added line width selecter

--- client/src/components/SettingsBar.jsx
+++ client/src/components/SettingsBar.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import toolState from "../store/toolState";
 import "../style/toolbar.scss";
 
 const SettingsBar = () => {
     return (
         <div className="settingsbar">
-            <p></p>
+            <label style={{ margin: "0 5px" }} htmlFor="line-width">
+                Line width
+            </label>
+            <input
+                onChange={(e) => toolState.setLineWidth(e.target.value)}
+                style={{ margin: "0 10px" }}
+                id="line-width"
+                type="number"
+                defaultValue={1}
+                min={1}
+                max={50}
+            />
         </div>
     );
 };
